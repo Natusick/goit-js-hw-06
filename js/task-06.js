56199fa08f475cfff504e305e23f59eb6192536d
Natusick <natallia.daniluk@gmail.com>
2022-01-22 10:47:33 +0200

    commit to task #6

--- js/task-06.js
+++ js/task-06.js
@@ -1,26 +1,10 @@
-const inputEl = document.querySelector('#validation-input');
+const inputEl = document.querySelector("#validation-input");
+const check = (event) => {
+  return event.target.value.length === Number(inputEl.dataset.length) ?
+      inputEl.setAttribute('class', 'valid') :
+      inputEl.setAttribute('class', 'invalid');
+}
+inputEl.addEventListener('blur', check);        
 
 
-
-inputEl.addEventListener('blur', event => {
-  event.preventDefault();
-  
-  if (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
-    
-      inputEl.classList.remove('invalid');
-    }
-    {
-    inputEl.classList.add('valid');
-    }
-
-  if (event.currentTarget.value.length ===! Number(inputEl.dataset.length)) {
-    
-      inputEl.classList.remove('valid');
-    }
-    {
-    inputEl.classList.add('invalid');
-    }  
-});
-
-
-
+ 
